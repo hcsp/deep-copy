a67bbbe6703f4abd0879386233a8b4832f0d78d0
Bo Zhang <bo@gradle.com>
2019-05-19 08:11:57 +0800

    Fix compilation errors

--- src/test/java/com/github/hcsp/MainTest.java
+++ src/test/java/com/github/hcsp/MainTest.java
@@ -2,13 +2,14 @@ package com.github.hcsp;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
+import com.github.hcsp.pet.Cat;
 import org.junit.jupiter.api.Test;
 
 public class MainTest {
     @Test
     public void deepCopyTest() {
         Home home = new Home(new Cat("Tom"));
-        Home deepCopy = deepCopy(home);
+        Home deepCopy = Main.deepCopy(home);
         assertFalse(home.cat == deepCopy.cat);
     }
 }
