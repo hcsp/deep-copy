faeb01cf67b75d0f945030f525ff9abd5ad88bb9
Zain <zainking2016@hotmail.com>
2019-06-09 01:31:04 +0800

    Finish deepCopy method

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -13,5 +13,14 @@ public class Main {
     }
     // Return the deep copy of a Home instance
     // 将传入的Home实例进行深拷贝，返回深拷贝后的实例
-    public static Home deepCopy(Home home) {}
+    public static Home deepCopy(Home home) {
+        Cat newCat;
+        if (home.cat != null) {
+            newCat = new Cat(home.cat.name);
+        } else {
+            newCat = new Cat("");
+        }
+        final Home newHome = new Home(newCat);
+        return newHome;
+    }
 }
