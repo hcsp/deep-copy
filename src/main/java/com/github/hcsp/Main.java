0d2b2047275eb2e4ca325160c4d61316d0f7fca8
catsme <845058547@qq.com>
2019-06-10 21:58:34 +0800

    完成深拷贝。

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -13,5 +13,12 @@ public class Main {
     }
     // Return the deep copy of a Home instance
     // 将传入的Home实例进行深拷贝，返回深拷贝后的实例
-    public static Home deepCopy(Home home) {}
+    public static Home deepCopy(Home home) {
+        Home deepcopy=new Home(new Cat(""));
+        Cat newcat=new Cat("");
+        String newname="false";
+        deepcopy.cat=newcat;
+        newcat.name=newname;
+        return deepcopy;
+    }
 }
