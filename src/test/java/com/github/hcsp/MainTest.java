9bec3e7ac6d46a585bf9aad4647fa51d2c857377
Bo Zhang <bo@gradle.com>
2019-06-15 18:43:07 +0800

    Update repo

--- src/test/java/com/github/hcsp/MainTest.java
+++ src/test/java/com/github/hcsp/MainTest.java
@@ -1,8 +1,7 @@
 package com.github.hcsp;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-
 import com.github.hcsp.pet.Cat;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class MainTest {
@@ -10,6 +9,6 @@ public class MainTest {
     public void deepCopyTest() {
         Home home = new Home(new Cat("Tom"));
         Home deepCopy = Main.deepCopy(home);
-        assertFalse(home.cat == deepCopy.cat);
+        Assertions.assertFalse(home.cat == deepCopy.cat);
     }
 }
