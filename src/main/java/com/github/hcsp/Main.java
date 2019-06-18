10eb978aed65816f150cc14d3e2120a1415ea1d7
huangjx <trustfor@sina.cn>
2019-06-18 11:14:03 +0800

    deep copy

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -13,5 +13,9 @@ public class Main {
     }
     // Return the deep copy of a Home instance
     // 将传入的Home实例进行深拷贝，返回深拷贝后的实例
-    public static Home deepCopy(Home home) {}
+    public static Home deepCopy(Home home) {
+        Cat bakCat = home.cat;
+        String bakName = bakCat.name;
+        return new Home(new Cat(bakName));
+    }
 }
