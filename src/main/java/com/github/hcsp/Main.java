73cedc69c6af1f02025d4bb798e1f4fd8d4c88c5
loirou <loirou@163.com>
2019-06-28 17:30:02 +0800

    修复深拷贝

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -14,7 +14,9 @@ public class Main {
     // Return the deep copy of a Home instance
     // 将传入的Home实例进行深拷贝，返回深拷贝后的实例
     public static Home deepCopy(Home home) {
-        Home home1 = home;
+        Cat cat = new Cat("");
+        cat = home.cat;
+        Home home1 = new Home(cat);
         return home1;
     }
 }
