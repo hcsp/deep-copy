758902cf3a6aa5f39b8f0b1644bb10e2fe5bef6e
sundljy <52794154+sundljy@users.noreply.github.com>
2019-07-16 01:37:02 +0800

    create function deep copy (#41)
    create function deep copy (#41)

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -11,7 +11,13 @@ public class Main {
         // 补全deepCopy()方法，使得程序输出"false"
         System.out.println(home.cat == deepCopy.cat);
     }
+
     // Return the deep copy of a Home instance
     // 将传入的Home实例进行深拷贝，返回深拷贝后的实例
-    public static Home deepCopy(Home home) {}
+    public static Home deepCopy(Home home) {
+        String name = home.cat.name;
+        Home newHome = new Home(new Cat(""));
+        newHome.cat.name = name;
+        return newHome;
+    }
 }
