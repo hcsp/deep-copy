1658b28d9417cc712a38d55d6249498f097d3957
wx85745997 <497045233@qq.com>
2019-07-27 18:02:07 +0800

    深拷贝

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -7,11 +7,15 @@ public class Main {
         Home home = new Home(new Cat("Tom"));
         Home deepCopy = deepCopy(home);
 
+
         // Complete deepCopy() method to make the program output "false"
         // 补全deepCopy()方法，使得程序输出"false"
         System.out.println(home.cat == deepCopy.cat);
     }
     // Return the deep copy of a Home instance
     // 将传入的Home实例进行深拷贝，返回深拷贝后的实例
-    public static Home deepCopy(Home home) {}
+    public static Home deepCopy(Home home) {
+        Home newHome = new Home(new Cat(home.cat.name));
+        return newHome;
+    }
 }
