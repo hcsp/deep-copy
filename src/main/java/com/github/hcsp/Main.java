3788124a7ea08a3e0bd8ee0e474f901e090467a4
uuflyer <49433104+uuflyer@users.noreply.github.com>
2019-08-02 11:46:16 +0800

    Tttest (#60)
    Tttest (#60)

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -13,5 +13,12 @@ public class Main {
     }
     // Return the deep copy of a Home instance
     // 将传入的Home实例进行深拷贝，返回深拷贝后的实例
-    public static Home deepCopy(Home home) {}
+    public static Home deepCopy(Home home) {
+        Home home1 = new Home(new Cat("Tom"));
+        Cat newcat = new Cat("Tom");
+        String newname = new String(home.cat.name);
+        home1.cat = newcat;
+        newcat.name = newname;
+        return home1;
+    }
 }
