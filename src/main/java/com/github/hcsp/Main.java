b7d42a1c917aacec76a779079d0fd84f53206f3e
gyd_lhw <834921748@qq.com>
2019-08-28 17:56:50 +0800

    deepcopy

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -13,5 +13,9 @@ public class Main {
     }
     // Return the deep copy of a Home instance
     // 将传入的Home实例进行深拷贝，返回深拷贝后的实例
-    public static Home deepCopy(Home home) {}
+    public static Home deepCopy(Home home) {
+        Cat cat1 = new Cat(home.cat.name);
+        Home home1 = new Home(cat1);
+        return home1;
+    }
 }
