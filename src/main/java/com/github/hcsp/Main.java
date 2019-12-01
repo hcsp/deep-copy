665026cc624d63761765d501dc3f5f6d829215f4
zhy533726735220 <zhy533726735220@163.com>
2019-12-01 15:18:55 +0800

    Java程序的基本结构练习：深拷贝 (#117)
    Java程序的基本结构练习：深拷贝 (#117)

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -13,5 +13,9 @@ public class Main {
     }
     // Return the deep copy of a Home instance
     // 将传入的Home实例进行深拷贝，返回深拷贝后的实例
-    public static Home deepCopy(Home home) {}
+    public static Home deepCopy(Home home) {
+        Cat copeCat = new Cat(home.cat.name);
+        Home copeHome = new Home(copeCat);
+        return copeHome;
+    }
 }
