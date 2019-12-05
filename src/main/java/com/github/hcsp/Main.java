964974e37b48c8333a856fb630f1187dd0680a8e
RunZe Zheng <58139571+zrzReyesTEI@users.noreply.github.com>
2019-12-05 21:47:29 +0800

    complete the method deepCopy (#122)
    complete the method deepCopy (#122)

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -13,5 +13,10 @@ public class Main {
     }
     // Return the deep copy of a Home instance
     // 将传入的Home实例进行深拷贝，返回深拷贝后的实例
-    public static Home deepCopy(Home home) {}
+    public static Home deepCopy(Home home) {
+        String newName = home.cat.name;
+        Cat newCat = new Cat(newName);
+        Home newHome = new Home(newCat);
+        return newHome;
+    }
 }
