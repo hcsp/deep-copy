7ade42b5124f34f5ff44b8d87dbba920f5ed29f9
Bing Qin <qinbing08@gmail.com>
2019-12-29 22:20:55 +0800

    深拷贝

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -14,6 +14,11 @@ public class Main {
     // Return the deep copy of a Home instance
     // 将传入的Home实例进行深拷贝，返回深拷贝后的实例
     public static Home deepCopy(Home home) {
-        return new Home(new Cat("mimi"));
+        Cat newCat = new Cat("Tom");
+        Home newHome = new Home(newCat);
+        String newName = home.cat.name;
+        newHome.cat = newCat;
+        newCat.name = newName;
+        return newHome;
     }
 }
