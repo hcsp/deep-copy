8a0aad0eab59ba43e11a03304e4d763737bfe372
郑海川 <53638659+lance-zheng66@users.noreply.github.com>
2020-03-29 12:43:26 +0800

    深层次拷贝 (#188)
    深层次拷贝 (#188)

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -13,5 +13,11 @@ public class Main {
     }
     // Return the deep copy of a Home instance
     // 将传入的Home实例进行深拷贝，返回深拷贝后的实例
-    public static Home deepCopy(Home home) {}
+    public static Home deepCopy(Home home) {
+        home.cat.name="tom";
+        Cat cat = new Cat("tom");
+
+        Home home1 = new Home(cat);
+        return home1;
+    }
 }
