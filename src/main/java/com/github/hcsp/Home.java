21761933590d01dd1d9e8a313d959e27af13e79f
yipinchen <haxilama110611@163.com>
2020-04-10 12:50:25 +0800

    深拷贝

--- src/main/java/com/github/hcsp/Home.java
+++ src/main/java/com/github/hcsp/Home.java
@@ -1,8 +1,10 @@
 package com.github.hcsp;
 
 import com.github.hcsp.pet.Cat;
+
 public class Home {
     public Cat cat;
+
     public Home(Cat cat) {
         this.cat = cat;
     }
--- src/main/java/com/github/hcsp/pet/Cat.java
+++ src/main/java/com/github/hcsp/pet/Cat.java
@@ -2,6 +2,7 @@ package com.github.hcsp.pet;
 
 public class Cat {
     public String name;
+
     public Cat(String name) {
         this.name = name;
     }
