39d72a3db3270b733893f8e8002fb91476c7ec74
dudueasy <dudueasy@126.com>
2021-01-17 20:55:20 +0800

    update

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -11,7 +11,12 @@ public class Main {
         // 补全deepCopy()方法，使得程序输出"false"
         System.out.println(home.cat == deepCopy.cat);
     }
+
     // Return the deep copy of a Home instance
     // 将传入的Home实例进行深拷贝，返回深拷贝后的实例
-    public static Home deepCopy(Home home) {}
+    public static Home deepCopy(Home home) {
+        String catName = home.cat.name;
+        Cat kitty = new Cat(catName);
+        return new Home(kitty);
+    }
 }
