dc4849a7abbeb0495eee51948c10c9e151cc3847
jcbound <784538725@qq.com>
2021-11-22 23:46:11 +0800

    进行深拷贝操作

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -5,6 +5,7 @@ import com.github.hcsp.pet.Cat;
 public class Main {
     public static void main(String[] args) {
         Home home = new Home(new Cat("Tom"));
+
         Home deepCopy = deepCopy(home);
 
         // Complete deepCopy() method to make the program output "false"
@@ -13,5 +14,9 @@ public class Main {
     }
     // Return the deep copy of a Home instance
     // 将传入的Home实例进行深拷贝，返回深拷贝后的实例
-    public static Home deepCopy(Home home) {}
+    public static Home deepCopy(Home home) {
+        Home newHome = new Home(new Cat("Tom"));
+        return newHome;
+
+    }
 }
