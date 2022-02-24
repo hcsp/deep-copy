ec3371d87e423ebffab32e47305a7dcca7aa9511
yezhiguang <99232516+yzgGrant@users.noreply.github.com>
2022-02-24 11:32:09 +0800

    深拷贝

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -13,5 +13,11 @@ public class Main {
     }
     // Return the deep copy of a Home instance
     // 将传入的Home实例进行深拷贝，返回深拷贝后的实例
-    public static Home deepCopy(Home home) {}
+    public static Home deepCopy(Home home) {
+        Home newHome = new Home();
+        Cat newCat = new Cat();
+        newCat.name = home.cat.name;
+        newHome.cat = newCat;
+        return newHome;
+    }
 }
