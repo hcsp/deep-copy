c2f5a83d41e023b87ee8c137d215a0da7b4082b4
Haohao98 <53430416+Haohao98@users.noreply.github.com>
2022-05-29 15:33:20 +0800

    deepcopy (#307)
    deepcopy (#307)

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -13,5 +13,10 @@ public class Main {
     }
     // Return the deep copy of a Home instance
     // 将传入的Home实例进行深拷贝，返回深拷贝后的实例
-    public static Home deepCopy(Home home) {}
+    public static Home deepCopy(Home home) {
+        String newstring  = home.cat.name;
+        Cat newcat = new Cat(newstring);
+        return  new Home(newcat);
+
+    }
 }
