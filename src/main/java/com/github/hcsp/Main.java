642a80584784d37b29ff3e60959c45a7f39b1754
lyra <lyraaaaaaaaaaa@gmail.com>
2025-07-07 21:09:39 +1000

    这个需要复习.

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -2,16 +2,25 @@ package com.github.hcsp;
 
 import com.github.hcsp.pet.Cat;
 
+
+
+// Complete deepCopy() method to make the program output "false"
+// Return the deep copy of a Home instance
+// 将传入的Home实例进行深拷贝，返回深拷贝后的实例
+
+
 public class Main {
     public static void main(String[] args) {
-        Home home = new Home(new Cat("Tom"));
+        Home home = new Home ( new Cat("Tom"));
         Home deepCopy = deepCopy(home);
-
-        // Complete deepCopy() method to make the program output "false"
-        // 补全deepCopy()方法，使得程序输出"false"
         System.out.println(home.cat == deepCopy.cat);
     }
-    // Return the deep copy of a Home instance
-    // 将传入的Home实例进行深拷贝，返回深拷贝后的实例
-    public static Home deepCopy(Home home) {}
-}
+
+    public static Home deepCopy(Home home) {
+        Cat copiedCat = new Cat(home.cat.name); // 创建一个新的 Cat 实例
+        Home copiedHome = new Home(copiedCat);  // 创建一个新的 Home 实例
+        return copiedHome;
+    }
+
+
+    }
